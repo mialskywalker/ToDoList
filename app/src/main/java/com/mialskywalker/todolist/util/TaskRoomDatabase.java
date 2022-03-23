68f6c99261cd56339ab3d945bd9bc18241e23191
mialskywalker <mialskywalker@gmail.com>
2022-03-23 18:54:40 +0200

    added TaskDao and Repository

---
+++ app/src/main/java/com/mialskywalker/todolist/util/TaskRoomDatabase.java
@@ -0,0 +1,56 @@
+package com.mialskywalker.todolist.util;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import com.mialskywalker.todolist.data.TaskDao;
+import com.mialskywalker.todolist.model.Task;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Task.class}, version = 1, exportSchema = false)
+public abstract class TaskRoomDatabase extends RoomDatabase {
+
+    public static final int NUMBER_OF_THREADS = 4;
+    public static final String DATABASE_NAME = "todolist_database";
+    private static volatile TaskRoomDatabase INSTANCE;
+    public static final ExecutorService databaseWriterExecutor
+            = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    public static final RoomDatabase.Callback sRoomDatabaseCallback =
+            new RoomDatabase.Callback() {
+                @Override
+                public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                    super.onCreate(db);
+                    databaseWriterExecutor.execute(() -> {
+                        TaskDao taskDao = INSTANCE.taskDao();
+                        taskDao.deleteAll();
+
+
+                    });
+                }
+            };
+
+    public static TaskRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (TaskRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            TaskRoomDatabase.class, DATABASE_NAME)
+                            .addCallback(sRoomDatabaseCallback)
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public abstract TaskDao taskDao();
+
+}
