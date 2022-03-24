47c7d7266058805b20be08526131372af0f537aa
mialskywalker <mialskywalker@gmail.com>
2022-03-24 15:26:01 +0200

    save button functionality added

--- app/src/main/java/com/mialskywalker/todolist/BottomSheetFragment.java
+++ app/src/main/java/com/mialskywalker/todolist/BottomSheetFragment.java
@@ -1,15 +1,40 @@
 package com.mialskywalker.todolist;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.chip.Chip;
+import com.mialskywalker.todolist.model.Priority;
+import com.mialskywalker.todolist.model.Task;
+import com.mialskywalker.todolist.model.TaskViewModel;
+
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.Group;
 import androidx.fragment.app.Fragment;
+
+import java.util.Calendar;
 //import androidx.navigation.fragment.NavHostFragment;
 
 public class BottomSheetFragment extends BottomSheetDialogFragment {
+
+    private EditText enterTodo;
+    private ImageButton calendarButton;
+    private ImageButton priorityButton;
+    private RadioGroup priorityRadioGroup;
+    private RadioButton selectedRadioButton;
+    private int selectedButtonId;
+    private ImageButton saveButton;
+    private CalendarView calendarView;
+    private Group calendarGroup;
 
     public BottomSheetFragment() {
 
@@ -21,12 +46,36 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
             Bundle savedInstanceState
     ) {
         View view = inflater.inflate(R.layout.bottom_sheet, container, false);
+        calendarGroup = view.findViewById(R.id.calendar_group);
+        calendarView = view.findViewById(R.id.calendar_view);
+        calendarButton = view.findViewById(R.id.today_calendar_button);
+        enterTodo = view.findViewById(R.id.enter_todo_et);
+        saveButton = view.findViewById(R.id.save_todo_button);
+        priorityButton = view.findViewById(R.id.priority_todo_button);
+        priorityRadioGroup = view.findViewById(R.id.radioGroup_priority);
+
+        Chip todayChip = view.findViewById(R.id.today_chip);
+        Chip tomorrowChip = view.findViewById(R.id.tomorrow_chip);
+        Chip nextWeekChip = view.findViewById(R.id.next_week_chip);
+
         return view;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        calendarButton.setOnClickListener(view12 -> {
+
+        });
+
+        saveButton.setOnClickListener(view1 -> {
+            String task = enterTodo.getText().toString().trim();
+            if (!TextUtils.isEmpty(task)) {
+                Task myTask = new Task(task, Priority.HIGH, Calendar.getInstance().getTime(),
+                        Calendar.getInstance().getTime(), false);
+                TaskViewModel.insert(myTask);
+            }
+        });
     }
 
 }
